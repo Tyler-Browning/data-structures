1b5d89efd63d4f26e9f086160d9a5f32867c4234
Tyler-Browning <111471846+Tyler-Browning@users.noreply.github.com>
2023-09-18 14:14:07 -0500

    weird

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -12,6 +13,30 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+       int num = 0;
+       for (String name : strings)
+       {
+        num++;
+       }
+
+       int len = num + 1;
+       String[] array = new String[len];
+
+       int g = num;
+       
+       ListIterator<String> i = strings.listIterator();
+       while (i.hasNext())
+       {
+            String s = i.next();
+            array[g] = s;
+            g--;
+            i.remove();
+       }
+
+       for (int q = num; q >= 0; q--)
+       {
+        strings.addFirst(array[q]);
+       }
+       
     }
 }
