70e18f0e5cb55f56a73fd96b859d7e4ba852bb61
Tyler-Browning <111471846+Tyler-Browning@users.noreply.github.com>
2023-09-21 14:12:51 -0500

    save 9/21

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -13,6 +13,33 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Read dictionary and novel
+        Set<String> dWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> nWords = readWords("Chapter 15 Class Notes/src/war-and-peace.txt");
+
+        // Print all novel words not in dictionary
+        //for (String word: nWords)
+        //{
+        //    if (!dWords.contains(word))
+        //    {
+        //        System.out.println(word);
+        //    }
+        //}
+
+        // Print number of unique words
+        //System.out.println("There are " + nWords.size() + " unique words in the novel");
+
+        // Print the number of unique words with greater than 3 letters
+        Iterator<String> i = nWords.iterator();
+        while (i.hasNext())
+        {
+            if (i.next().length() > 3)
+            {
+                i.remove();
+            }
+        }
+        System.out.println("There are " + nWords.size() + " unique words with more than 3 letters in the novel");
+
     }
 
     /**
@@ -25,6 +52,22 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        Set<String> words = new HashSet<>();
+
+        // Determine the current directory
+        //System.out.println(System.getProperty("user.dir"));
+
+        Scanner in = new Scanner (new File(filename), "UTF-8");
+
+        // Use any char that is not a letter as a delimeter
+        in.useDelimiter("[^a-zA-Z]+");
+
+        while(in.hasNext())
+        {
+            // Add words to the set (Dupes ignored)
+            words.add(in.next().toLowerCase());
+        }
+
+        return words;
     }
 }
