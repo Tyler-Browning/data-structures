fa2c4acd79dec042b687b085baa118f1dcec29d5
Tyler-Browning <111471846+Tyler-Browning@users.noreply.github.com>
2023-09-28 14:03:59 -0500

    Mapssss

--- Chapter 15 Class Notes/src/MapDemo.java
+++ Chapter 15 Class Notes/src/MapDemo.java
@@ -11,6 +11,26 @@ public class MapDemo
 {
     public static void main(String[] args)
     {
-        
+        // Map interface generic
+        // Type for key and value
+        Map<String, Color> fav = new HashMap <>();
+
+        // Add elements using the put method
+        fav.put("Peter", Color.BLACK);
+        fav.put("Seth", Color.BLUE);
+        fav.put("Celeste", Color.ORANGE);
+
+        // Two diffrent keys can have the same value
+        fav.put("Miller", Color.ORANGE);
+
+        // The same key cannot have two values
+        fav.put("Peter", Color.YELLOW); // Replaces the old color (Black --> Yellow)
+
+        // Create a set of the keys in a map
+        Set <String> keys = fav.keySet();
+        for (String key : keys)
+        {
+            System.out.println(key + " ("+key.hashCode()+"): " + fav.get(key));
+        }
     }
 }
